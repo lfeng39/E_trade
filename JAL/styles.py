9e936cc74a74fba4d67a07ffa539550d708a0b80
lfeng39 <lfeng0309@gmail.com>
2022-07-03 21:12:04 +0800

    js

--- JAL/styles.py
+++ JAL/styles.py
@@ -13,9 +13,9 @@ def position():
 def bgContainer():
 
     bgContainer = {
-        models.asin[0]: 'background: url(' + models.imgShower[1] + ') no-repeat; background-position: 0 -200px; background-size: 100%; height: 790px;',
-        models.asin[1]: 'background: url(' + models.imgShower[2] + ') no-repeat; background-size: 100%; min-height: 790px;',
-        models.asin[2]: 'background: url(' + models.imgShower[0] + '); background-size: 100%; min-height: 790px;',
+        models.asin[0]: 'background: url(' + models.imgShower[1] + ') no-repeat; background-size: 100%; height: 790px;',
+        models.asin[1]: 'background: url(' + models.imgShower[2] + ') no-repeat; background-size: 100%; height: 790px;',
+        models.asin[2]: 'background: url(' + models.imgShower[0] + '); background-size: 100%; height: 790px;',
     }
 
     return bgContainer
--- static/js/lfeng.js
+++ static/js/lfeng.js
@@ -12,3 +12,40 @@ window.addEventListener('load', function()
         )
     }
 )
+
+window.onload = function()
+{
+    var bodyWid = document.body.clientWidth
+    var widB09YLLXKDT = document.getElementById('B09YLLXKDT').clientWidth * 0.6
+    var widB09YLKWBMV = document.getElementById('B09YLKWBMV').clientWidth * 0.6
+    var widB09KG4R3YR = document.getElementById('B09KG4R3YR').clientWidth * 0.6
+    var heiB09YLLXKDT = widB09YLLXKDT + 'px'
+    var heiB09YLKWBMV = widB09YLKWBMV + 'px'
+    var heiB09KG4R3YR = widB09KG4R3YR + 'px'
+    if(bodyWid < 1280)
+    {
+        document.getElementById('B09YLLXKDT').style.height = heiB09YLLXKDT
+        document.getElementById('B09YLKWBMV').style.height = heiB09YLKWBMV
+        document.getElementById('B09KG4R3YR').style.height = heiB09KG4R3YR
+    }
+}        
+
+function myfunction()
+{
+    console.log(document.body.clientWidth)
+    var bodyWid = document.body.clientWidth
+    var widB09YLLXKDT = document.getElementById('B09YLLXKDT').clientWidth * 0.6
+    var widB09YLKWBMV = document.getElementById('B09YLKWBMV').clientWidth * 0.6
+    var widB09KG4R3YR = document.getElementById('B09KG4R3YR').clientWidth * 0.6
+    var heiB09YLLXKDT = widB09YLLXKDT + 'px'
+    var heiB09YLKWBMV = widB09YLKWBMV + 'px'
+    var heiB09KG4R3YR = widB09KG4R3YR + 'px'
+    if(bodyWid < 1280)
+    {
+        document.getElementById('B09YLLXKDT').style.height = heiB09YLLXKDT
+        document.getElementById('B09YLKWBMV').style.height = heiB09YLKWBMV
+        document.getElementById('B09KG4R3YR').style.height = heiB09KG4R3YR
+    }
+}
+
+window.addEventListener("resize", myfunction);
