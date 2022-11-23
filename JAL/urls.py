b8c711b1463da49fe24dae93b0916d8b95970f36
lfeng39 <lfeng39@163.com>
2022-11-23 13:40:33 +0800

    branch v1_00

--- JAL/urls.py
+++ JAL/urls.py
@@ -1,20 +1,48 @@
-from django.urls import path
-
+from urllib import request
+from django.urls import path, re_path
+from django.shortcuts import render, HttpResponse, redirect
 from JAL import views
+from JAL import data_source
 from JAL import models
 
+
+
+# def login(request):  # login函数
+#     if request.method == "GET":  # 前端如果是get请求
+#         return render(request, 'login.html')  # 返回HTML页面。
+#     elif request.method == "POST":  # 前端如果是post请求
+#         username = request.POST.get("username")  # 获取POST请求中的username值
+#         password = request.POST.get("password")  # 获取密码值
+#         if username == "zy" and password == "12345":
+#             return redirect("/index/")    #重定向到index页面
+#         else:  # 如果用户名或者密码错误，返回登录页面
+#             return render(request, "login.html")
+
+# def index(request):  # index函数
+#     return render(request, "index.html")
+
+# B0BFHQTG6R == _asin_[0]
+# asin = 'B09YLLXKDT'
+# ['B0BFHQTG6R', 'B09KG4R3YR', 'B09YLKWBMV', 'B09YLLXKDT']
+
+
+
+
 app_name = 'jal'
+
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
-    path('index', views.index, name='index'),
-    path('about',views.about, name='about'),
-    path('product',views.products, name='product'),
-    path('B09YLLXKDT',views.zmh, name='zmh'),
-    path('B09YLKWBMV',views.ydj, name='ydj'),
-    path('B09KG4R3YR',views.ddl, name='ddl'),
-    path('passWord',views.test, name='test'),
-    path('yes',views.postData, name='test'),
-    path('login',views.login, name='login'),
-    path('signUp',views.signUp, name='signUp'),
+    # url 按顺序查找页面
+    path('', views._index_, name=''),
+    path('about',views._about_, name=''),
+    path('products',views._products_, name=''),
+    path('products/<asin_transfer>',views._detail_, name=''),
+    path('login',views._login_, name=''),
+    path('signUp',views.signUp, name=''),
+    path('cart',views.myCart, name=''),
+    path('order',views._order_, name=''),
+    path('account',views._account_, name=''),
+    path('myAccount',views.myAccount, name=''),
+    path('yes',views.postData, name=''),
+    path('test',models.verifyAccount, name=''),
 ]
