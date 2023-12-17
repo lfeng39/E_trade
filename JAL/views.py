91daad1aa8bbb49d7c6f817e4ef785d9571732e1
lfeng39@163.com <lfeng0309@gmail.com>
2023-12-17 22:33:41 +0800

    account coupon module update

--- JAL/views.py
+++ JAL/views.py
@@ -50,7 +50,7 @@ _ip_ = '127.0.0.1'
 # csrftoken: T83BR0wnzOOGoGNuSw3mw9kOyQWif8Ns
 _port_ = ':8000'
 _app_ = '/JAL/'
-base_url = http + _ip_ + _port_ + _app_
+# base_url = http + _ip_ + _port_ + _app_
 '''
 server test url
 '''
@@ -58,7 +58,7 @@ server test url
 # _ip_ = '822u770q09.zicp.fun:44088'
 # ngrok
 _ip_ = '6c7d-103-84-219-16.ngrok-free.app'
-# base_url = 'https://' + _ip_ + '/JAL/'
+base_url = 'https://' + _ip_ + '/JAL/'
 '''
 Vultr server url
 '''
